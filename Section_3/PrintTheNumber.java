5cbc335d8ea6097fe3c83b8266284789e68c5857
Sourav Kumar <xsouravkumar357@gmail.com>
2025-05-19 20:36:31 -0700

    Print numbers

---
+++ Section_3/PrintTheNumber.java
@@ -0,0 +1,9 @@
+package Section_3;
+
+public class PrintTheNumber {
+    public static void main(String[] args) {
+        
+        int myFirstNumber = 5;
+        System.out.println("My first Number is " + myFirstNumber);
+    }
+}
--- Section_3/Program1.java
+++
@@ -1,9 +0,0 @@
-package Section_3;
-
-public class Program1 {
-    
-    public static void main(String[] args) {
-        
-        System.out.println("Hello world");
-    }
-}
