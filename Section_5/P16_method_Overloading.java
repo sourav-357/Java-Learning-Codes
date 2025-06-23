5a2567f6fbef29dc74604486d7f17c38e6e517c0
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-23 21:22:55 +0530

    Method overloading challenge

--- Section_5/P16_method_Overloading.java
+++ Section_5/P16_method_Overloading.java
@@ -6,22 +6,30 @@ public class P16_method_Overloading {
      public static void main(String[] args) {
 
           // Using the first method we just created
-          int newScore = calculateScore("abc", 500);
-          System.out.println("New score is " + newScore);
+          System.out.println("New score is " + calculateScore("abc", 500));
 
           // using the second method we created with different parameters -->> Method overloading
+          System.out.println("New Score for the unnamed player is " + calculateScore(200));
+
+          // using the third method we created with different parameters -->> Method overloading
           System.out.println("New Score for the unnamed player is " + calculateScore(200));
      }
 
      // now creating a new method 
      public static int calculateScore(String playerName, int score) {
-          System.out.println("Player " + playerName + " scored " + score + " points");
+          System.out.println("\nPlayer " + playerName + " scored " + score + " points");
           return score * 1000;
      }
 
-     // again creating a new method 
+     // again creating a new method with same name -->> Method overloading
      public static int calculateScore(int score) {
-          System.out.println("Unnamed player scored " + score + " points");
+          System.out.println("\nUnnamed player scored " + score + " points");
           return score * 1000;
+     }
+
+     // again creating a new method with same name -->> Method overloading
+     public static int calculateScore() {
+          System.out.println("\nNo player name, no player score");
+          return 0;
      }
 }
