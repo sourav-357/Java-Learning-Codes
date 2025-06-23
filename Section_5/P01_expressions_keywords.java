432af718ba808e9d36950428da957273568ea523
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-23 07:52:33 +0530

    Expressions and keywords

---
+++ Section_5/P01_expressions_keywords.java
@@ -0,0 +1,25 @@
+// the reserved keywords and expressions
+
+package Section_5;
+
+public class P01_expressions_keywords {
+     public static void main(String[] args) {
+          // int int = 5; // wrong -->> as int is a reserved keywords
+
+          int int2 = 5;
+          System.out.println(int2);
+          System.out.println("Hello from the Code");
+
+          int highscore = 50;
+          if (highscore > 25) {
+               highscore = highscore + 1000; // Bonus points added
+          }
+          System.out.println("Your score is " + highscore);
+
+          int health = 90;
+          if ((health < 25) && (health > 1000)) {
+               highscore = highscore - 1000;
+          }
+          System.out.println("Your score now is " + highscore);
+     }
+}
