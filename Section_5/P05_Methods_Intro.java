de7fbd6efee6cdf36d18b9d8733ba79618871c51
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-23 08:54:38 +0530

    Methods introduction

---
+++ Section_5/P05_Methods_Intro.java
@@ -0,0 +1,28 @@
+package Section_5;
+
+public class P05_Methods_Intro {
+
+     public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+          int finalScore = score;
+          if (gameOver) {
+               finalScore += (levelCompleted * bonus);
+               System.out.println("Your final Score was " + finalScore);
+          }
+     }
+     public static void main(String[] args) {
+          
+          boolean gameOver = true;
+          int score = 800;
+          int levelCompleted = 5;
+          int bonus = 100;
+          
+          // Callimg the method we just created
+          System.out.println("This is the first method of calculating scores");
+          calculateScore(gameOver, score, levelCompleted, bonus);
+
+          // Calling the same method again 
+          System.out.println("This is for the second values of calculating scores");
+          calculateScore(true, 1000, 10, 200);
+     }
+}
