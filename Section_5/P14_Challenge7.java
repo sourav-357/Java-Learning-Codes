2bc08e0cf92fb08ea8ca91e00266467ba14598f4
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-23 19:44:34 +0530

    Challenge 7 completed

---
+++ Section_5/P14_Challenge7.java
@@ -0,0 +1,20 @@
+/*
+Write a method hasEqualSum with 3 parameters of type int.
+
+The method should return boolean and it needs to return true if the sum of the first and second parameter is equal to the third parameter. Otherwise, return false.
+*/
+package Section_5;
+
+public class P14_Challenge7 {
+
+     public static boolean hasEqualSum(int num1, int num2, int num3) {
+          if ((num1 + num2) == num3) {
+               return true;
+          } else {
+               return false;
+          }
+     }
+     public static void main(String[] args) {
+          
+     }
+}
