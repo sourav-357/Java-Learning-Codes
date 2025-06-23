646c546689c59218cec6c9b88c9a0b8f9ba5d200
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-23 15:43:38 +0530

    Challenge 1

---
+++ Section_5/P08_Challenge1.java
@@ -0,0 +1,32 @@
+/*
+Write a method called checkNumber with an int parameter named number.
+The method should not return any value, and it needs to print out:
+- "positive" if the parameter number is > 0
+- "negative" if the parameter number is < 0
+- "zero" if the parameter number is equal to 0
+*/
+
+package Section_5;
+import java.util.Scanner;
+
+public class P08_Challenge1 {
+
+     // Creating a method to check the number 
+     public static void checkNumber(int number) {
+          if (number > 0) {
+               System.out.println("Positive");
+          } else if(number < 0) {
+               System.out.println("negative");
+          } else {
+               System.out.println("zero");
+          }
+     }
+     public static void main(String[] args) {
+          
+          Scanner sc = new Scanner(System.in);
+          System.out.println("Please enter the number you need to check: ");
+          int num = sc.nextInt();
+          checkNumber(num);
+          sc.close();
+     }
+}
