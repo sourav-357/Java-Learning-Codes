23880225e906f0896821338ec2d923a9dfc411b8
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-23 08:34:41 +0530

    Challenge completed

--- Section_5/P04_if_else_Challenge.java
+++ Section_5/P04_if_else_Challenge.java
@@ -4,14 +4,25 @@ public class P04_if_else_Challenge {
      public static void main(String[] args) {
           
           boolean gameOver = true;
-          int score = 2000;
+          int score = 800;
           int levelCompleted = 5;
           int bonus = 100;
 
           int finalScore = score;
-          if (gameOver == true) {
+          if (gameOver) {
                finalScore += (levelCompleted * bonus);
                System.out.println("Your final Score was " + finalScore);
+          }
+
+          //-------------------- replacing the variables with new values --------------------//
+          
+          score = 10000; // we don't have to re-define these variables
+          levelCompleted = 8; // we don't have to re-define these variables
+          bonus = 200; // we don't have to re-define these variables
+
+          if (gameOver) {
+               finalScore += (levelCompleted * bonus);
+               System.out.println("Your final Score now is " + finalScore);
           }
      }
 }
