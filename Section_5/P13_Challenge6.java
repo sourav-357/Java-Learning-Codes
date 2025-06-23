9f190feb16c1da06d11271b6f326e362cce1210a
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-23 19:40:25 +0530

    Challenge 6 completed

---
+++ Section_5/P13_Challenge6.java
@@ -0,0 +1,15 @@
+/*
+Write a method areEqualByThreeDecimalPlaces with two parameters of type double.
+The method should return boolean and it needs to return true if two double numbers are the same up to three decimal places. Otherwise, return false.
+*/
+package Section_5;
+
+public class P13_Challenge6 {
+
+     public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
+          return (((long)(num1 * 1000)) == ((long)(num2 * 1000)));
+     }
+     public static void main(String[] args) {
+          boolean isTrue = areEqualByThreeDecimalPlaces(1.998, 1.998);
+     }
+}
