7efd0c5b9a9ec46b27957c6404293bcc2f9f2541
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-23 08:04:22 +0530

    White space and expressions

---
+++ Section_5/P02_Statements_Whitespeace.java
@@ -0,0 +1,27 @@
+package Section_5;
+
+public class P02_Statements_Whitespeace {
+     public static void main(String[] args) {
+          
+          // Defining a new variable
+          int myvariable = 50;
+          myvariable--;
+          myvariable++;
+
+          // the expression is still executable
+          System.out.println("This is a test");
+          System.out.println("This is just " + 
+          "another " + 
+          "Test");
+
+          // White Spacing in Java
+          System.out.println
+          (
+               "the variable is " + myvariable
+               )
+               ;
+
+          // Giving multiple expressions on a single line 
+          int anotherVariable = 50; myvariable--; System.out.println("my variable " + myvariable + " and another variable " + anotherVariable);
+     }
+}
