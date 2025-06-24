1cabcdf60de774941bddf29ac0d5da5cb2e8d8f8
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-25 00:37:20 +0530

    Taking input from the console

---
+++ Section_6/P26_Taking_Inputs.java
@@ -0,0 +1,22 @@
+package Section_6;
+
+public class P26_Taking_Inputs {
+
+     // The main file to run the codes
+     public static void main(String[] args) {
+          int currentYear = 2022;
+          System.out.println(getInputFromConsole(currentYear));
+          System.out.println(getInputFromScanner(currentYear));
+     }
+     // Creating the first method to take inputs from the console
+     public static String getInputFromConsole(int currentYear) {
+          String name = System.console().readLine("Hi, What's your Name? ");   // Taking inputs
+          System.out.println("Hi " + name + ", Thanks for taking the course!");  // Printing the taken inputs
+          String dateOfBirth = System.console().readLine("What year were you born? ");   // Taking inputs
+          int age = currentYear - Integer.parseInt(dateOfBirth);  
+          return "So you are " + age + " years old";   // Printing the taken inputs
+     }
+     public static String getInputFromScanner(int currentYear) {
+          return "";
+     }
+}
