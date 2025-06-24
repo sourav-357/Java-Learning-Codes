73ba8c3123fc5bd75dd7d3f72a3d45791c3e6355
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-24 23:45:26 +0530

    Challenge 22

--- Section_6/P20_Challenge22.java
+++ Section_6/P20_Challenge22.java
@@ -1,8 +1,5 @@
 /*
-Write a method named getGreatestCommonDivisor with two parameters of type int named first and second.
-If one of the parameters is < 10, the method should return -1 to indicate an invalid value.
-The method should return the greatest common divisor of the two numbers (int).
-The greatest common divisor is the largest positive integer that can fully divide each of the integers (i.e. without leaving a remainder). 
+
 */
 package Section_6;
 
