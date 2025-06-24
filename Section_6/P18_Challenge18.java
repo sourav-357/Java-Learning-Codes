f6d3a060b4476a97eecce5b803d0675d8ea2ebd0
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-24 23:18:47 +0530

    Challenge 18

--- Section_6/P18_Challenge18.java
+++ Section_6/P18_Challenge18.java
@@ -1,5 +1,7 @@
 /*
-
+Write a method named getEvenDigitSum with one parameter of type int called number. 
+The method should return the sum of the even digits within the number. 
+If the number is negative, the method should return -1 to indicate an invalid value.
 */
 package Section_6;
 
