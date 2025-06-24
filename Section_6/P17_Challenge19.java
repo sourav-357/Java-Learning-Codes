16957c8706acc59c6db459f32ad28d03b5736ed2
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-24 23:25:09 +0530

    Challenge 19

--- Section_6/P17_Challenge19.java
+++ Section_6/P17_Challenge19.java
@@ -1,5 +1,8 @@
 /*
-
+Write a method named hasSharedDigit with two parameters of type int. 
+Each number should be within the range of 10 (inclusive) - 99 (inclusive). 
+If one of the numbers is not within the range, the method should return false.
+The method should return true if there is a digit that appears in both numbers, such as 2 in 12 and 23; otherwise, the method should return false.
 */
 package Section_6;
 
