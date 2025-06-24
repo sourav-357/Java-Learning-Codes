0b43cad6ba952a596e6f12df2ceca1137d3211ce
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-24 23:35:49 +0530

    Challenge 20

--- Section_6/P18_Challenge20.java
+++ Section_6/P18_Challenge20.java
@@ -1,8 +1,5 @@
 /*
-Write a method named hasSameLastDigit with three parameters of type int. 
-Each number should be within the range of 10 (inclusive) - 1000 (inclusive). 
-If one of the numbers is not within the range, the method should return false.
-The method should return true if at least two of the numbers share the same rightmost digit; otherwise, it should return false.
+
 
 Write another method named isValid with one parameter of type int.
 The method needs to return true if the number parameter is in range of 10(inclusive) - 1000(inclusive), otherwise return false.
