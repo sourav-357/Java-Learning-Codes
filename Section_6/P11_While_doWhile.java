a1b18f391e33d9589c9a48cb87a2b728ebb72235
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-24 22:23:02 +0530

    Do while loop

---
+++ Section_6/P11_While_doWhile.java
@@ -0,0 +1,20 @@
+package Section_6;
+
+public class P11_While_doWhile {
+     public static void main(String[] args) {
+          for (int i = 1; i <= 5; i++) {
+               System.out.println(i);
+          }
+          int j = 1;
+          boolean isReady = false;
+          do {
+               if (j > 5) {
+                    break;
+               }
+               System.out.println(j);
+               j++;
+               isReady = (j > 0);
+          } while (isReady);
+          
+     }
+}
