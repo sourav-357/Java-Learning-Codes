c22b3a9272f98124d792e8f87a52d89190eda309
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-24 23:15:35 +0530

    Challenge 17

--- Section_6/P15_Challenge17.java
+++ Section_6/P15_Challenge17.java
@@ -1,5 +1,8 @@
 /*
-
+Write a method named sumFirstAndLastDigit with one parameter of type int called number. 
+The method needs to find the first and the last digit of the parameter number passed to the method, 
+using a loop and return the sum of the first and the last digit of that number.
+If the number is negative then the method needs to return -1 to indicate an invalid value.
 */
 package Section_6;
 
