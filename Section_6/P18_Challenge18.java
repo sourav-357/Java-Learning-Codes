8d425d57caeeefe936e57cb619560b5418358fcc
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-24 23:18:32 +0530

    Challenge 18

---
+++ Section_6/P18_Challenge18.java
@@ -0,0 +1,26 @@
+/*
+
+*/
+package Section_6;
+
+public class P18_Challenge18 {
+     public static int getEvenDigitSum(int number) {
+          if (number < 0) {
+               return -1; // Invalid input
+          }
+          int sum = 0;
+          while (number > 0) {
+               int digit = number % 10;
+               if (digit % 2 == 0) {
+                    sum += digit;
+               }
+               number /= 10;
+          }
+          return sum;
+     }
+     public static void main(String[] args) {
+          System.out.println(getEvenDigitSum(123456789)); // 2 + 4 + 6 + 8 = 20
+          System.out.println(getEvenDigitSum(252));       // 2 + 2 = 4
+          System.out.println(getEvenDigitSum(-22));       // -1 (invalid)
+     }
+}
