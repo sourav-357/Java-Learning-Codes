c694ee787148dac0a6410aa579b0216ae2e7fdf5
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-24 23:14:24 +0530

    Challenge 16

---
+++ Section_6/P14_Challenge16.java
@@ -0,0 +1,30 @@
+/*
+Write a method called isPalindrome with one int parameter called number.
+The method needs to return a boolean.
+It should return true if the number is a palindrome number otherwise it should return false. 
+Check the tips below for more info about palindromes.
+*/
+package Section_6;
+
+public class P14_Challenge16 {
+
+     public static boolean isPalindrome(int number) {
+          int originalNumber = number;
+          int reversed = 0;
+
+          // Make the number positive for reversal logic
+          number = Math.abs(number);
+          while (number > 0) {
+               int lastDigit = number % 10;
+               reversed = reversed * 10 + lastDigit;
+               number /= 10;
+          }
+          // Compare reversed with original number (ignoring sign)
+          return Math.abs(originalNumber) == reversed;
+     }
+     public static void main(String[] args) {
+          System.out.println(isPalindrome(121));   // true
+          System.out.println(isPalindrome(-121));  // true
+          System.out.println(isPalindrome(123));   // false
+     }
+}
