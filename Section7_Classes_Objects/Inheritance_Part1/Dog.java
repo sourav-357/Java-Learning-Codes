53d00915de2a060984165b8d99ed46423d784f3f
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-27 01:11:26 +0530

    Inheritance completion

---
+++ Section7_Classes_Objects/Inheritance_Part1/Dog.java
@@ -0,0 +1,8 @@
+package Section7_Classes_Objects.Inheritance_Part1;
+
+public class Dog extends Animal {
+
+    public Dog() {
+        super("Mutt", "Big", 50);
+    }
+}
---
+++ Section7_Classes_Objects/Inheritance_Part1/Main.java
@@ -0,0 +1,21 @@
+package Section7_Classes_Objects.Inheritance_Part1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animal animal = new Animal("Generic Animal", "Huge", 400);
+        doAnimalStuff(animal, "slow");
+
+        Dog dog = new Dog();
+        doAnimalStuff(dog, "fast");
+    }
+
+    public static void doAnimalStuff(Animal animal, String speed) {
+
+        animal.makeNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("_ _ _ _");
+    }
+}
