d8e882a690153e3ff4270b38b1b31e285b267c70
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-26 23:04:37 +0530

    Constructor part 2 complete

---
+++ Section7_Classes_Objects/Constructor_part2/Main.java
@@ -0,0 +1,26 @@
+package Section7_Classes_Objects.Constructor_part2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Account bobsAccount = new Account();
+
+        System.out.println(bobsAccount.getNumber());
+        System.out.println(bobsAccount.getBalance());
+        
+        bobsAccount.withdrawFunds(100.0);
+        bobsAccount.depositFunds(250);
+        bobsAccount.withdrawFunds(50);
+
+        bobsAccount.withdrawFunds(200);
+
+        bobsAccount.depositFunds(100);
+        bobsAccount.withdrawFunds(45.55);
+        bobsAccount.withdrawFunds(54.46);
+
+        bobsAccount.withdrawFunds(54.45);
+        Account timsAccount = new Account("Tim", "tim@email.com", "12345");
+        System.out.println("AccountNo: " + timsAccount.getNumber() + "; name " + timsAccount.getCustomerName());
+    }
+}
