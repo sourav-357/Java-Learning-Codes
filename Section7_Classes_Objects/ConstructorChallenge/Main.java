f1f4894bddebecf2a62f6c8b8b60eacd141132b8
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-26 23:55:41 +0530

    Constructor challenge

--- Section7_Classes_Objects/ConstructorChallenge/Main.java
+++ Section7_Classes_Objects/ConstructorChallenge/Main.java
@@ -3,8 +3,7 @@ package Section7_Classes_Objects.ConstructorChallenge;
 public class Main {
      public static void main(String[] args) {
 
-        Customer customer = new Customer("Tim", 1000,
-                "tim@email.com");
+        Customer customer = new Customer("Tim", 1000, "tim@email.com");
         System.out.println(customer.getName());
         System.out.println(customer.getCreditLimit());
         System.out.println(customer.getEmail());
