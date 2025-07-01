ea622324066b191c90db9b3cbc150672dabfd3ee
Sourav Kumar <xsouravkumar357@gmail.com>
2025-07-01 08:32:09 +0530

    Child class for the inheritance

---
+++ Section8_Encapsulation_polymorphism/ChildClass/Child.java
@@ -0,0 +1,18 @@
+package Section8_Encapsulation_polymorphism.ChildClass;
+
+// Child class
+class Cat extends Animal {
+     public void greeting() {
+          System.out.println("The cat meows.");
+     }
+
+     public void voice2() {
+          System.out.println("Meow");
+     }
+
+     public static void main(String[] args) {
+          Animal cat1 = new Cat(); // Cat object
+          cat1.greeting(); // prints "The cat meows." from current class, BUT:
+          cat1.voice1(); // if methods are different i can access only the parent method!!
+     }
+}
