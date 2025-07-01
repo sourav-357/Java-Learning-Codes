3ffa57e842e57bfc4eec8c1a5d66f50a57e1667a
Sourav Kumar <xsouravkumar357@gmail.com>
2025-07-01 08:36:49 +0530

    Ceiling part success

---
+++ Section8_Encapsulation_polymorphism/Coding_exercise_37/Ceiling.java
@@ -0,0 +1,19 @@
+package Section8_Encapsulation_polymorphism.Coding_exercise_37;
+
+public class Ceiling {
+     private int height;
+     private int paintedColor;
+
+     public Ceiling(int height, int paintedColor) {
+          this.height = height;
+          this.paintedColor = paintedColor;
+     }
+
+     public int getHeight() {
+          return height;
+     }
+
+     public int getPaintedColor() {
+          return paintedColor;
+     }
+}
