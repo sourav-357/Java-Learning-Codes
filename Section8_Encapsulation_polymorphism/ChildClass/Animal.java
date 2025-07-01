d5b0eb9e84446f4aae66288faac9d1d6e3ef54c1
Sourav Kumar <xsouravkumar357@gmail.com>
2025-07-01 08:31:21 +0530

    Animal class from the inheritance

---
+++ Section8_Encapsulation_polymorphism/ChildClass/Animal.java
@@ -0,0 +1,12 @@
+package Section8_Encapsulation_polymorphism.ChildClass;
+
+// Parent class
+class Animal {
+     public void greeting() {
+          System.out.println("The animal greets you.");
+     }
+
+     public void voice1() {
+          System.out.println("Noise!");
+     }
+}
