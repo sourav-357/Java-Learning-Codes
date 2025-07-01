17297a41cab521af00f3ed6c6340b7a15e1e0277
Sourav Kumar <xsouravkumar357@gmail.com>
2025-07-01 08:45:18 +0530

    Garland Lights part done successfully

---
+++ Section8_Encapsulation_polymorphism/Composition_Room/GarlandLights.java
@@ -0,0 +1,29 @@
+package Section8_Encapsulation_polymorphism.Composition_Room;
+
+public class GarlandLights {
+     private String color;
+     private boolean isWorking;
+     private float length;
+
+     public GarlandLights(String color, boolean isWorking, float length) {
+          this.color = color;
+          this.isWorking = isWorking;
+          this.length = length;
+     }
+
+     public void pplsReaction() {
+          System.out.println("Girls go squiiiirt!");
+     }
+
+     public String getColor() {
+          return color;
+     }
+
+     public boolean isWorking() {
+          return isWorking;
+     }
+
+     public float getLength() {
+          return length;
+     }
+}
