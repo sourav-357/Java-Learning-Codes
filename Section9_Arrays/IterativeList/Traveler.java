23864c779099351d576d643f1dc5ea9f765629a8
Sourav Kumar <xsouravkumar357@gmail.com>
2025-07-02 08:36:13 +0530

    Traveller done Completely

---
+++ Section9_Arrays/IterativeList/Traveler.java
@@ -0,0 +1,31 @@
+package Section9_Arrays.IterativeList;
+
+public class Traveler {
+    private String travelerName;
+    private int travelerAge;
+    private static int numberVisitedCities;
+
+    public Traveler(String travelerName, int travelerAge) {
+        this.travelerName = travelerName;
+        this.travelerAge = travelerAge;
+    }
+
+    public Traveler() {
+    }
+
+    public String getTravelerName() {
+        return travelerName;
+    }
+
+    public int getTravelerAge() {
+        return travelerAge;
+    }
+
+    public static int getNumberVisitedCities() {
+        return numberVisitedCities;
+    }
+
+    public static void setNumberVisitedCities(int numberVisitedCities) {
+        Traveler.numberVisitedCities = numberVisitedCities;
+    }
+}
