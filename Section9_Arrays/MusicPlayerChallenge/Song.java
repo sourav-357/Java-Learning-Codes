518ddd310c78fa7048dd79eeec2dd2b29cbbcc56
Sourav Kumar <xsouravkumar357@gmail.com>
2025-07-02 08:43:55 +0530

    Music Player Challenge Done

---
+++ Section9_Arrays/MusicPlayerChallenge/Song.java
@@ -0,0 +1,28 @@
+package Section9_Arrays.MusicPlayerChallenge;
+
+/*the most basic class*/
+public class Song {
+     private String title;
+     private double duration;
+
+     public String getTitle() {
+          return title;
+     }
+
+     public double getDuration() {
+          return duration;
+     }
+
+     public Song(String title, double duration) {
+          this.title = title;
+          this.duration = duration;
+     }
+
+     @Override
+     public String toString() {
+          return "Song{" +
+                    "title='" + title + '\'' +
+                    ", duration=" + duration +
+                    '}';
+     }
+}
