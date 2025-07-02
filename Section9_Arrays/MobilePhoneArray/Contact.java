dab8d6c1727ec0fa922d2eae1af477dfa3798586
Sourav Kumar <xsouravkumar357@gmail.com>
2025-07-02 08:23:02 +0530

    Contact part of the mobile phone done

---
+++ Section9_Arrays/MobilePhoneArray/Contact.java
@@ -0,0 +1,25 @@
+package Section9_Arrays.MobilePhoneArray;
+
+//this class is a blueprint for myContacs ArrayList from the class MobilePhone
+
+public class Contact {
+     private String name = new String();
+     private String phoneNumber = new String();
+
+     public Contact(String name, String phoneNumber) {
+          this.name = name;
+          this.phoneNumber = phoneNumber;
+     }
+
+     public String getName() {
+          return name;
+     }
+
+     public String getPhoneNumber() {
+          return phoneNumber;
+     }
+
+     static public Contact createContact(String personName, String phoneNumber) {
+          return new Contact(personName, phoneNumber); // initiating a new instance
+     }
+}
